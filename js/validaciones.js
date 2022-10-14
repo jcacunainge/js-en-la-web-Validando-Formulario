85f3d557803a3d021644181e52c354a45355cfd6
Jhan Carlos Acuña Ordoñez <jcacunainge@gmail.com>
2022-10-14 16:19:43 -0500

    comiit 2

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,1 +1,12 @@
-const inputNacimiento = document.querySelector('#birth');
+//console.log('validaciones');
+const inputNacimiento = document.querySelector('#birth')
+
+inputNacimiento.addEventListener("blur", (evento) => {
+  console.log(evento.target);
+})
+
+//function validarNacimiento(input){
+   // const fecha = input.evento;
+ //   console.log(fecha);}
+
+//
